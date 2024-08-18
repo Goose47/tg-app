c7d24e5ef8aecbc2ea58185a0bf3cfb93b38c86c
Nikita <tyumin000@gmail.com>
2024-08-18 17:06:31 +0300

    removed fastapi and added telebot

--- api/main.py
+++ api/main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+import telebot
+from telebot import types
+
+load_dotenv()
+
+telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+web_app_url = os.getenv('WEB_APP_URL')
+
+bot = telebot.TeleBot()
 
 
-app = FastAPI()
+@bot.message_handler(commands = ['start'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    webUrl = types.WebAppInfo(web_app_url)
+    web_btn = types.InlineKeyboardButton(text='НАЖМИ!', web_app=webUrl)
+    markup.add(web_btn)
+    bot.send_message(message.from_user.id, "Давай давай оп оп", reply_markup = markup)
 
 
-@app.get("/")
-async def get():
-    return 'Hello, world!'
+if __name__ == '__main__':
+    bot.infinity_polling()
